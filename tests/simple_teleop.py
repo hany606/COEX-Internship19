017c4e5825a24a3ba9a443f698b892f97b25655c
hany606 <h.hamed.elanwar@gmail.com>
2019-07-12 15:30:14 +0300

    Add tests and organize the projects

--- tests/simple_teleop.py
+++ tests/simple_teleop.py
@@ -14,6 +14,9 @@ import settings
 target_x = 3
 target_y = 3
 out_flag = 0
+return_track_flag = 0
+moves_record = []
+key_change = 0
 
 rospy.init_node("simple_teleop_node") # name of your ROS node
 
@@ -31,6 +34,45 @@ arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
 # Safety check for the error of throttling speed
 shared.safety_check(get_telemetry, arming)
 
+def memorize_move(target_x, target_y):
+    # Take the advantage of lazy evaluation of python
+    if(len(moves_record) == 0 or moves_record[-1] != (target_x,target_y)):
+        global moves_record
+        moves_record.append((target_x, target_y))
+
+def key_callback(data):
+    #print(type(data.data))
+    s = data.data
+    tmp_x = 0
+    tmp_y = 0
+    global key_change
+    key_change = 1
+    if(s == 'w'):
+        print("Forward")
+        tmp_y = 1
+    elif(s == 's'):
+        print("Backward")
+        tmp_y = -1
+    elif(s == 'd'):
+        print("Right")
+        tmp_x = 1
+    elif(s == 'a'):
+        print("Left")
+        tmp_x = -1
+    elif(s == 'm'):
+        print("Return the track")
+        global return_track_flag
+        return_track_flag = 1
+    elif(s == 'q'):
+        print("Break")
+        global out_flag
+        out_flag = 1
+    global target_x
+    global target_y
+    global moves_record
+    target_x = min(max(target_x+tmp_x,settings.ArUcoMAPMIN_x),settings.ArUcoMAPMAX_x)
+    target_y = min(max(target_y+tmp_y,settings.ArUcoMAPMIN_y),settings.ArUcoMAPMAX_y)
+    memorize_move(target_x, target_y)
 def drone_land():
     print("Landing")
 
@@ -51,48 +93,15 @@ def take_off():
     print("Height: ~1.5m")
     navigate(z=settings.VIEW_HIGHT, speed=settings.SPEED, frame_id='body', auto_arm=True)
     shared.wait_till_arrive(get_telemetry, 0, 0, start_pos.z+settings.VIEW_HIGHT, 'map',1)
-    # count_down(3)
-    # current_pos = get_telemetry("aruco_map")
-    # navigate(x=current_pos.x, y=current_pos.y, z=current_pos.z, speed=settings.SPEED, frame_id='aruco_map')
-    # shared.wait_till_arrive(get_telemetry,  current_pos.x, current_pos.y, current_pos.z)
 
-def task1(target_x, target_y):
-    # target_x = 3
-    # target_y = 3
+def move(target_x, target_y):
     current_pos = get_telemetry(frame_id='aruco_map')
     navigate(x=target_x, y=target_y, z=current_pos.z, yaw=float('nan'), speed=settings.SPEED, frame_id='aruco_map')
     shared.wait_till_arrive(get_telemetry, target_x, target_y, current_pos.z)
 
-def key_callback(data):
-    #print(type(data.data))
-    s = data.data
-    tmp_x = 0
-    tmp_y = 0
-    if(s == 'w'):
-        print("Forward")
-        tmp_x = 1
-    elif(s == 's'):
-        print("Backward")
-        tmp_x = -1
-    elif(s == 'd'):
-        print("Right")
-        tmp_y = 1
-    elif(s == 'a'):
-        print("Left")
-        tmp_y = -1
-    elif(s == 'q'):
-        print("Break")
-        global out_flag
-        out_flag = 1
-    global target_x
-    global target_y
-    target_x = min(max(target_x+tmp_x,settings.ArUcoMAPMIN_x),settings.ArUcoMAPMAX_x)
-    target_y = min(max(target_y+tmp_y,settings.ArUcoMAPMIN_y),settings.ArUcoMAPMAX_y)
-
 def main():
-    
     take_off()
-    task1(3,2)
+    move(target_x,target_y)
     print("Ready to be controlled from keyboard")
     rospy.Subscriber("key_value", String, key_callback)
     print("Get ready in:")
@@ -101,8 +110,24 @@ def main():
     while(out_flag == 0):
         global target_x
         global target_y
-        task1(target_x,target_y)
-        print(target_x, target_y, out_flag) 
+        global key_change
+        if(key_change):
+            if(return_track_flag):
+                global moves_record
+                for i in range(2, len(moves_record)+1):
+                    print("Return Return Return to:")
+                    move(moves_record[-i][0], moves_record[-i][1])
+                    print(moves_record[-i][0], moves_record[-i][1])
+                global target_x
+                global target_y
+                target_x,target_y = moves_record[0]
+            else:
+                move(target_x,target_y)
+                print(target_x, target_y, out_flag)
+            global key_change
+            key_change = 0
+            
+
 
     drone_land()
 
